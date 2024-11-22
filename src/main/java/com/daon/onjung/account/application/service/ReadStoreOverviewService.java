86577dda9db898f135885417a403c16b5cfdf4cd
EunJiJung <bianbbc87@gmail.com>
2024-11-22 16:29:35 +0900

    ♻️ refactor/#54 : 테스트용 주석 추가
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/main/java/com/daon/onjung/account/application/service/ReadStoreOverviewService.java
+++ src/main/java/com/daon/onjung/account/application/service/ReadStoreOverviewService.java
@@ -71,7 +71,7 @@ public class ReadStoreOverviewService implements ReadStoreOverviewUseCase {
         int end = Math.min(start + pageable.getPageSize(), storeOverviewDtos.size());
         List<ReadStoreOverviewsResponseDto.StoreOverviewDto> pagedStoreOverviewDtos = storeOverviewDtos.subList(start, end);
 
-        // 응답 생성
+        // 응답 생성 - 변경
         return ReadStoreOverviewsResponseDto.fromEntity(
                 pagedStoreOverviewDtos,
                 end < storeList.getTotalElements() // hasNext 계산
